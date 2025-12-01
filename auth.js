7ee7393738ee3af23d22bf004014c098b7b1f817
D0SANJ0S <marcelinhoreis32@gmail.com>
2025-12-01 16:09:18 -0300

    auth.js

--- auth.js
+++ auth.js
@@ -1,19 +1,29 @@
+import { auth } from "./firebase.js";
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-auth.js";
+
 function register() {
     const user = document.getElementById("regUser").value;
     const pass = document.getElementById("regPass").value;
 
-    const users = JSON.parse(localStorage.getItem("users") || "[]");
+    try {
+        await createUserWithEmailAndPassword(auth, user, pass);
 
-    if (users.find(u => u.user === user)) {
-        alert("Usuário já existe!");
-        return;
+        alert("Conta criada com sucesso!");
+        window.location = "index.html";
+
+    } catch (error) {
+        console.error(error);
+
+        if (error.code === "auth/email-already-in-use") {
+            alert("Este login já está registrado.");
+        } else if (error.code === "auth/invalid-email") {
+            alert("login inválido.");
+        } else if (error.code === "auth/weak-password") {
+            alert("A senha deve ter pelo menos 6 caracteres.");
+        } else {
+            alert("Erro ao criar a conta.");
+        }
     }
-
-    users.push({ user, pass });
-    localStorage.setItem("users", JSON.stringify(users));
-
-    alert("Conta criada!");
-    window.location = "index.html";
 }
 
 function login() {
@@ -37,3 +47,4 @@ function logout() {
     localStorage.removeItem("loggedUser");
     window.location = "index.html";
 }
+
